279d762337cca1480ab5edf009347c410aac13dd
emmanuel-dev3 <chukwuemmanuel042419@gmail.com>
2025-02-14 04:07:33 +0100

    description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import FramerWrapper from "@/components/animation/FramerWrapper";
 
 export const siteConfig = {
   name: "Emmanuel Chukwu",
-  description: "I am a Passionate Software Developer",
+  description: "I am a Passionate Web Developer",
   ogImage: "../../public/screenshot.png",
   url: "https://portfolio-dun-psi-86.vercel.app/",
 };
